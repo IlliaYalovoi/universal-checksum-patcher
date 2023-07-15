f2ccac2a016b3d70a9e05cc26aae44708763ca5a
Illia Yalovoi <imperio091@gmail.com>
2023-07-15 11:15:12 +0300

    Some fixes

--- main.go
+++ main.go
@@ -7,14 +7,37 @@ import (
 	"github.com/manifoldco/promptui"
 )
 
+var debugMode = false
+
 func main() {
 
-	prompt := promptui.Select{
-		Label: "Select game to patch",
-		Items: []string{"Europa Universalis IV", "Hearts of Iron IV"},
+	OS := runtime.GOOS
+
+	if debugMode {
+
+		promptOSChoice := promptui.Select{
+			Label:    fmt.Sprintf("Choose desired OS"),
+			Items:    []string{"Windows", "Linux", "Darwin"},
+			HideHelp: true,
+		}
+		_, desiredOS, _ := promptOSChoice.Run()
+
+		switch desiredOS {
+		case "windows":
+			OS = "windows"
+		case "linux":
+			OS = "linux"
+		case "darwin":
+			OS = "darwin"
+		}
+	}
+	promptGame := promptui.Select{
+		Label:    "Select game to patch",
+		Items:    []string{"Europa Universalis IV", "Hearts of Iron IV"},
+		HideHelp: true,
 	}
 
-	_, result, err := prompt.Run()
+	_, result, err := promptGame.Run()
 
 	if err != nil {
 		fmt.Printf("Prompt failed %v\n", err)
@@ -23,9 +46,9 @@ func main() {
 
 	switch result {
 	case "Europa Universalis IV":
-		err = applyPatch(false, "eu4", runtime.GOOS)
+		err = applyPatch(debugMode, "eu4", OS)
 	case "Hearts of Iron IV":
-		err = applyPatch(false, "hoi4", runtime.GOOS)
+		err = applyPatch(debugMode, "hoi4", OS)
 	}
 
 	if err != nil {
--- test_eu4_patch_test.go
+++ test_eu4_patch_test.go
@@ -6,45 +6,65 @@ import (
 )
 
 func TestApplyPatchEU4Windows(t *testing.T) {
-	err := backupFile("./test_files/eu4/eu4_original.exe", "./test_files/eu4/eu4_test.exe")
+	err := backupFile("./test_files/eu4/eu4_windows_original.exe", "./test_files/eu4/eu4_windows_test.exe")
 
 	if err != nil {
 		t.Error(err)
 	}
-	err = applyPatch(true, "./test_files/eu4/eu4_test", "windows")
+	err = applyPatch(true, "./test_files/eu4/eu4_windows_test", "windows")
 
 	if err != nil {
 		t.Error(err)
 	}
 
-	if !compareExes("./test_files/eu4/eu4_patched.exe", "./test_files/eu4/eu4_test.exe") {
+	if !compareExes("./test_files/eu4/eu4_windows_patched.exe", "./test_files/eu4/eu4_windows_test.exe") {
 		if err != nil {
 			t.Error("Executables doesnt match")
 		}
 	}
-	os.Remove("./test_files/eu4/eu4_test.exe")
-	os.Remove("./test_files/eu4/eu4_test_backup.exe")
-	os.Remove("./test_files/eu4/eu4.exe")
+	os.Remove("./test_files/eu4/eu4_windows_test.exe")
+	os.Remove("./test_files/eu4/eu4_windows_test_backup.exe")
+	os.Remove("./test_files/eu4/eu4_windows.exe")
 }
 
 func TestApplyPatchEU4Linux(t *testing.T) {
-	err := backupFile("./test_files/eu4/eu4_original", "./test_files/eu4/eu4_test")
+	err := backupFile("./test_files/eu4/eu4_linux_original", "./test_files/eu4/eu4_linux_test")
 
 	if err != nil {
 		t.Error(err)
 	}
-	err = applyPatch(true, "./test_files/eu4/eu4_test", "linux")
+	err = applyPatch(true, "./test_files/eu4/eu4_linux_test", "linux")
 	if err != nil {
 		t.Error(err)
 	}
 
-	if !compareExes("./test_files/eu4/eu4_patched", "./test_files/eu4/eu4_test") {
+	if !compareExes("./test_files/eu4/eu4_linux_patched", "./test_files/eu4/eu4_linux_test") {
 		if err != nil {
 			t.Error("Executables doesnt match")
 		}
 	}
-	os.Remove("./test_files/eu4/eu4_test")
-	os.Remove("./test_files/eu4/eu4_test_backup")
-	os.Remove("./test_files/eu4/eu4")
+	os.Remove("./test_files/eu4/eu4_linux_test")
+	os.Remove("./test_files/eu4/eu4_linux_test_backup")
+	os.Remove("./test_files/eu4/eu4_linux")
+}
 
+func TestApplyPatchEU4Darwin(t *testing.T) {
+	err := backupFile("./test_files/eu4/eu4_darwin_original", "./test_files/eu4/eu4_darwin_test")
+
+	if err != nil {
+		t.Error(err)
+	}
+	err = applyPatch(true, "./test_files/eu4/eu4_darwin_test", "darwin")
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !compareExes("./test_files/eu4/eu4_darwin_patched", "./test_files/eu4/eu4_darwin_test") {
+		if err != nil {
+			t.Error("Executables doesnt match")
+		}
+	}
+	os.Remove("./test_files/eu4/eu4_darwin_test")
+	os.Remove("./test_files/eu4/eu4_darwin_test_backup")
+	os.Remove("./test_files/eu4/eu4_darwin")
 }
