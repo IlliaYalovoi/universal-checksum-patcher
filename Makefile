95681e9142c52cb4ecbbf13a3f13c60365ee4091
Illia Yalovoi <imperio091@gmail.com>
2023-11-02 10:00:33 +0200

    hoi4 1.13.5 updated

--- Makefile
+++ Makefile
@@ -1,13 +1,4 @@
-all: build-windows build-linux build-macos-amd64 build-macos-arm64
+all: build-windows
 
 build-windows:
-	env GOOS=windows GOARCH=amd64 go build -o ./build/universal-checksum-patcher-windows.exe *.go 
-
-build-linux:
-	env GOOS=linux GOARCH=amd64 go build -o ./build/universal-checksum-patcher-linux *.go 
-
-build-macos-amd64:
-	env GOOS=darwin GOARCH=amd64 go build -o ./build/universal-checksum-patcher-macos-amd64 *.go
-
-build-macos-arm64:
-	env GOOS=darwin GOARCH=arm64 go build -o ./build/universal-checksum-patcher-macos-arm64 *.go 
+	env GOOS=windows GOARCH=amd64 go build -o ./build/universal-checksum-patcher-windows.exe *.go
--- test_eu4_patch_test.go
+++ test_eu4_patch_test.go
@@ -11,60 +11,26 @@ func TestApplyPatchEU4Windows(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
-	err = applyPatch(true, "./test_files/eu4/eu4_windows_test", "windows")
+	err = applyPatch("./test_files/eu4/eu4_windows_test", "windows")
 
 	if err != nil {
 		t.Error(err)
 	}
 
 	if !compareExes("./test_files/eu4/eu4_windows_patched.exe", "./test_files/eu4/eu4_windows_test.exe") {
-		if err != nil {
-			t.Error("Executables doesnt match")
-		}
+		t.Error("Executables doesnt match")
 	}
-	os.Remove("./test_files/eu4/eu4_windows_test.exe")
-	os.Remove("./test_files/eu4/eu4_windows_test_backup.exe")
-	os.Remove("./test_files/eu4/eu4_windows.exe")
-}
 
-func TestApplyPatchEU4Linux(t *testing.T) {
-	err := backupFile("./test_files/eu4/eu4_linux_original", "./test_files/eu4/eu4_linux_test")
-
+	err = os.Remove("./test_files/eu4/eu4_windows_test.exe")
 	if err != nil {
-		t.Error(err)
+		t.Error("Cannot delete temp files")
 	}
-	err = applyPatch(true, "./test_files/eu4/eu4_linux_test", "linux")
+	err = os.Remove("./test_files/eu4/eu4_windows_test_backup.exe")
 	if err != nil {
-		t.Error(err)
+		t.Error("Cannot delete temp files")
 	}
-
-	if !compareExes("./test_files/eu4/eu4_linux_patched", "./test_files/eu4/eu4_linux_test") {
-		if err != nil {
-			t.Error("Executables doesnt match")
-		}
-	}
-	os.Remove("./test_files/eu4/eu4_linux_test")
-	os.Remove("./test_files/eu4/eu4_linux_test_backup")
-	os.Remove("./test_files/eu4/eu4_linux")
-}
-
-func TestApplyPatchEU4Darwin(t *testing.T) {
-	err := backupFile("./test_files/eu4/eu4_darwin_original", "./test_files/eu4/eu4_darwin_test")
-
+	err = os.Remove("./test_files/eu4/eu4_windows.exe")
 	if err != nil {
-		t.Error(err)
+		t.Error("Cannot delete temp files")
 	}
-	err = applyPatch(true, "./test_files/eu4/eu4_darwin_test", "darwin")
-	if err != nil {
-		t.Error(err)
-	}
-
-	if !compareExes("./test_files/eu4/eu4_darwin_patched", "./test_files/eu4/eu4_darwin_test") {
-		if err != nil {
-			t.Error("Executables doesnt match")
-		}
-	}
-	os.Remove("./test_files/eu4/eu4_darwin_test")
-	os.Remove("./test_files/eu4/eu4_darwin_test_backup")
-	os.Remove("./test_files/eu4/eu4_darwin")
 }
